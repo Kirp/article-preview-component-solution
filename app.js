eb017636950cb327e492e24cabdfea925935b887
Kierro DeCampanille <pentawick@gmail.com>
2023-02-24 14:58:04 +0800

    v1 prep for submission

--- app.js
+++ app.js
@@ -1,14 +1,68 @@
 function swapshare(){
-    //console.log(document.getElementById('switcher-button').style.filter);
-    if(document.getElementById('switcher-button').style.filter == "none" || document.getElementById('switcher-button').style.filter == "")
+    if(window.innerWidth<1000)
     {
-        document.getElementById('social-links').style.bottom = "0";
-        document.getElementById('switcher-button').style.filter = "brightness(0) saturate(100%) invert(97%) sepia(11%) saturate(87%) hue-rotate(303deg) brightness(115%) contrast(100%)";
-        document.getElementById('switcher-button').style.rotate = "360deg";
-    }else
+        //drag up the social
+        if(document.getElementById('switcher-button-arrow').style.filter == "none" || document.getElementById('switcher-button-arrow').style.filter == "")
+        {
+            document.getElementById('social-links').style.visibility = "visible";
+            document.getElementById('social-links').style.opacity = "1";
+            document.getElementById('social-links').style.left = "0";
+            document.getElementById('social-links').style.bottom = "0";
+            document.getElementById('switcher-button-arrow').style.filter = "brightness(0) saturate(100%) invert(97%) sepia(11%) saturate(87%) hue-rotate(303deg) brightness(115%) contrast(100%)";
+            document.getElementById('switcher-button').style.rotate = "360deg";
+        }else
+        {
+            document.getElementById('social-links').style.bottom = "-5.5em";
+            document.getElementById('switcher-button-arrow').style.filter = "none";
+            document.getElementById('switcher-button').style.rotate = "-360deg";
+        }
+    }else 
     {
-        document.getElementById('social-links').style.bottom = "-5.5em";
-        document.getElementById('switcher-button').style.filter = "none";
-        document.getElementById('switcher-button').style.rotate = "-360deg";
+        //pop up the social;
+        if(document.getElementById('switcher-button-arrow').style.filter == "none" || document.getElementById('switcher-button-arrow').style.filter == "")
+        {
+            document.getElementById('social-links').style.left = "16.75em";
+            document.getElementById('social-links').style.bottom= "7em";
+            document.getElementById('social-links').style.visibility = "visible";
+            document.getElementById('social-links').style.opacity = "1";
+            document.getElementById('switcher-button').style.backgroundColor = "hsla(212, 23%, 69%, 100%)";
+            document.getElementById('switcher-button-arrow').style.filter = "brightness(0) saturate(100%) invert(97%) sepia(11%) saturate(87%) hue-rotate(303deg) brightness(115%) contrast(100%)";
+            document.getElementById('switcher-button').style.rotate = "360deg";
+        }else
+        {
+            document.getElementById('social-links').style.visibility = "hidden";
+            document.getElementById('social-links').style.opacity = "0";
+            document.getElementById('switcher-button').style.backgroundColor = "hsla(212, 23%, 69%, 40%)";
+            document.getElementById('switcher-button-arrow').style.filter = "none";
+            document.getElementById('switcher-button').style.rotate = "-360deg";
+        }
     }
 }
+
+function windowIsResizing(){
+    // in case of resizing lets auto hide the social link to avoid having issues with it
+    document.getElementById('social-links').style.visibility = "hidden";
+    document.getElementById('social-links').style.opacity = "0";
+    if(window.innerWidth<1000)
+    {
+        //drag up the social
+        if(document.getElementById('switcher-button-arrow').style.filter != "none")
+        {   
+            document.getElementById('social-links').style.bottom = "-5.5em";
+            document.getElementById('switcher-button-arrow').style.filter = "none";
+            document.getElementById('switcher-button').style.rotate = "-360deg";
+        }
+    }else 
+    {
+        //pop up the social;
+        if(document.getElementById('switcher-button-arrow').style.filter != "none")
+        {   
+            document.getElementById('switcher-button').style.backgroundColor = "hsla(212, 23%, 69%, 40%)";
+            document.getElementById('switcher-button-arrow').style.filter = "none";
+            document.getElementById('switcher-button').style.rotate = "-360deg";
+        }
+    }
+}
+
+//add a resize listener
+window.addEventListener('resize', windowIsResizing);
